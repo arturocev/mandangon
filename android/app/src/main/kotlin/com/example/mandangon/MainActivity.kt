f6eb7d88a7532b1b18e28344f311f3d173b2d798
Arturo alumno 516 <arturo_garcia_m-tsapp1oa2324@cev.com>
2025-03-05 10:13:48 +0100

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/mandangon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mandangon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mandangon',
+      home: const MyHomePage(title: 'Mandangon'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Inicio",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: "Recetas",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant),
+            label: "Restaurantes",
+          ),
+        ],
+        onTap: (index) {
+          // Navegación entre pantallas
+        },
+      ),
+    );
+  }
+}
