89b822e35119a8197e3a42b9e5725c0b887af4c8
AlexCev <alejandroruiz21203@gmail.com>
2025-03-11 12:26:54 +0100

    2025-03-11 Conexion a base de datos realizada

--- lib/crear_receta_screen.dart
+++ lib/crear_receta_screen.dart
@@ -9,7 +9,7 @@ import 'package:flutter/foundation.dart';
 class CrearRecetaScreen extends StatefulWidget {
   final Map<String, String>? receta;
 
-  const CrearRecetaScreen({Key? key, this.receta}) : super(key: key);
+  const CrearRecetaScreen({super.key, this.receta});
 
   @override
   CrearRecetaScreenState createState() => CrearRecetaScreenState();
@@ -28,11 +28,15 @@ class CrearRecetaScreenState extends State<CrearRecetaScreen> {
   @override
   void initState() {
     super.initState();
-    tituloController = TextEditingController(text: widget.receta?['titulo'] ?? '');
+    tituloController =
+        TextEditingController(text: widget.receta?['titulo'] ?? '');
     tipoController = TextEditingController(text: widget.receta?['tipo'] ?? '');
-    ingredientesController = TextEditingController(text: widget.receta?['ingredientes'] ?? '');
-    instruccionesController = TextEditingController(text: widget.receta?['instrucciones'] ?? '');
-    tiempoController = TextEditingController(text: widget.receta?['tiempo'] ?? '');
+    ingredientesController =
+        TextEditingController(text: widget.receta?['ingredientes'] ?? '');
+    instruccionesController =
+        TextEditingController(text: widget.receta?['instrucciones'] ?? '');
+    tiempoController =
+        TextEditingController(text: widget.receta?['tiempo'] ?? '');
     imagenPath = widget.receta?['imagen'];
   }
 
@@ -68,14 +72,16 @@ class CrearRecetaScreenState extends State<CrearRecetaScreen> {
     }
 
     if (kIsWeb) {
-      return Image.network(imagenPath!, width: double.infinity, height: 200, fit: BoxFit.cover);
+      return Image.network(imagenPath!,
+          width: double.infinity, height: 200, fit: BoxFit.cover);
     } else {
-      return Image.file(File(imagenPath!), width: double.infinity, height: 200, fit: BoxFit.cover);
+      return Image.file(File(imagenPath!),
+          width: double.infinity, height: 200, fit: BoxFit.cover);
     }
   }
 
-  Future<void> guardarReceta() async {
-    if (!_formKey.currentState!.validate()) return;
+  void guardarReceta() async {
+    /*if (!_formKey.currentState!.validate()) return;*/
 
     setState(() {
       subiendo = true;
@@ -87,38 +93,47 @@ class CrearRecetaScreenState extends State<CrearRecetaScreen> {
       imagenBase64 = base64Encode(imageBytes);
     }
 
-    Map<String, dynamic> recetaData = {
+    var url = Uri.parse("http://localhost/guardar_receta.php");
+
+    http.Response respuesta = await http.post(url, body: {
       'rec_nom': tituloController.text,
       'rec_tipo_com': tipoController.text,
       'rec_ing': ingredientesController.text,
       'rec_desc': instruccionesController.text,
       'rec_tmp': tiempoController.text,
       'rec_img': imagenBase64 ?? '',
-      'rec_usu': 'usuario_demo'  
-    };
+      'rec_usu': 'usuario_demo'
+    });
 
-    var response = await http.post(
-      Uri.parse("http://localhost/mandangon/guardar_receta.php"),
-      body: jsonEncode(recetaData),
-    );
+    if (respuesta.statusCode == 200) {
+      print("Conexion exitosa");
+    } else {
+      print("Conexion fallida");
+    }
+/*
+    if (!mounted) return; // Evita usar context si el widget ha sido desmontado
 
     setState(() {
       subiendo = false;
     });
 
-    var respuestaServidor = jsonDecode(response.body);
+    var respuestaServidor = jsonDecode(respuesta.body);
     if (respuestaServidor["success"]) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Receta guardada con éxito")));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Receta guardada con éxito")));
       Navigator.pop(context);
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error: ${respuestaServidor["message"]}")));
-    }
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error: ${respuestaServidor["message"]}")));
+    }*/
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.receta == null ? "Nueva Receta" : "Editar Receta")),
+      appBar: AppBar(
+          title:
+              Text(widget.receta == null ? "Nueva Receta" : "Editar Receta")),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -136,22 +151,50 @@ class CrearRecetaScreenState extends State<CrearRecetaScreen> {
                   ),
                 ),
                 SizedBox(height: 20),
-                _campoTexto(label: "Título", controller: tituloController, hintText: "Ejemplo: Tarta de chocolate", inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^[a-zA-Z\s]+$'))]),
+                _campoTexto(
+                    label: "Título",
+                    controller: tituloController,
+                    hintText: "Ejemplo: Tarta de chocolate",
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                          RegExp(r'^[a-zA-Z\s]+$'))
+                    ]),
                 SizedBox(height: 15),
-                _campoTexto(label: "Tipo de comida", controller: tipoController, hintText: "Ejemplo: Postre", inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^[a-zA-Z\s]+$'))]),
+                _campoTexto(
+                    label: "Tipo de comida",
+                    controller: tipoController,
+                    hintText: "Ejemplo: Postre",
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                          RegExp(r'^[a-zA-Z\s]+$'))
+                    ]),
                 SizedBox(height: 15),
-                _campoTexto(label: "Ingredientes", controller: ingredientesController, hintText: "Ejemplo: 2 huevos, 200g de harina...", maxLines: 3),
+                _campoTexto(
+                    label: "Ingredientes",
+                    controller: ingredientesController,
+                    hintText: "Ejemplo: 2 huevos, 200g de harina...",
+                    maxLines: 3),
                 SizedBox(height: 15),
-                _campoTexto(label: "Instrucciones", controller: instruccionesController, hintText: "Escribe los pasos de la receta...", maxLines: 5),
+                _campoTexto(
+                    label: "Instrucciones",
+                    controller: instruccionesController,
+                    hintText: "Escribe los pasos de la receta...",
+                    maxLines: 5),
                 SizedBox(height: 15),
-                _campoTexto(label: "Tiempo estimado (minutos)", controller: tiempoController, hintText: "Ejemplo: 30", inputFormatters: [FilteringTextInputFormatter.digitsOnly]),
+                _campoTexto(
+                    label: "Tiempo estimado (minutos)",
+                    controller: tiempoController,
+                    hintText: "Ejemplo: 30",
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly]),
                 SizedBox(height: 20),
                 Center(
                   child: subiendo
                       ? CircularProgressIndicator()
                       : ElevatedButton(
                           onPressed: guardarReceta,
-                          child: Text(widget.receta == null ? 'Guardar Receta' : 'Actualizar Receta'),
+                          child: Text(widget.receta == null
+                              ? 'Guardar Receta'
+                              : 'Actualizar Receta'),
                         ),
                 ),
               ],
@@ -162,14 +205,21 @@ class CrearRecetaScreenState extends State<CrearRecetaScreen> {
     );
   }
 
-  Widget _campoTexto({required String label, required TextEditingController controller, required String hintText, List<TextInputFormatter>? inputFormatters, int maxLines = 1}) {
+  Widget _campoTexto(
+      {required String label,
+      required TextEditingController controller,
+      required String hintText,
+      List<TextInputFormatter>? inputFormatters,
+      int maxLines = 1}) {
     return TextFormField(
       controller: controller,
       maxLines: maxLines,
       inputFormatters: inputFormatters,
-      decoration: InputDecoration(labelText: label, hintText: hintText, border: OutlineInputBorder()),
+      decoration: InputDecoration(
+          labelText: label, hintText: hintText, border: OutlineInputBorder()),
       validator: (value) {
-        if (value == null || value.trim().isEmpty) return "Este campo es obligatorio";
+        if (value == null || value.trim().isEmpty)
+          return "Este campo es obligatorio";
         return null;
       },
     );
